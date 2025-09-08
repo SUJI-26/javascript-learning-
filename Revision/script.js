e1104f4fd69844e692bc5e6c673fd0de58bd0c8a
Suji V <Suji26262@gmail.com>
2025-09-08 19:23:15 +0530

    today change

---
+++ Revision/script.js
@@ -0,0 +1,15 @@
+// // let name = "Suji";
+// // let age = 20;
+// // let showname = ` My name is ${name}, My age is ${age}`
+// // console.log(showname)
+
+// // let num  ="123";
+// // console.log(Number(num))
+// let age = 22;
+
+// if (age >= 18) {
+//     console.log("Adult");
+// } else {
+//     console.log("Minor");
+// }
+
